8ade019b784025a2c3ca2c9f60a23ee7d3daea39
Irwan Fathurrahman <meomancer@gmail.com>
2023-08-22 13:44:35 +0700

    Implement help system on frontend

---
+++ django_project/frontend/src/components/HelpCenter/Block.jsx
@@ -0,0 +1,139 @@
+/**
+ * GeoSight is UNICEF's geospatial web-based business intelligence platform.
+ *
+ * Contact : geosight-no-reply@unicef.org
+ *
+ * .. note:: This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation; either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ * __author__ = 'irwan@kartoza.com'
+ * __date__ = '22/08/2023'
+ * __copyright__ = ('Copyright 2023, Unicef')
+ */
+
+import React, { Fragment, useEffect, useState } from 'react';
+import CircularProgress from "@mui/material/CircularProgress";
+import { ArrowForwardIcon } from "../Icons";
+
+/** Block help. **/
+export default function Block({ data, isRoot }) {
+  const [loading, setLoading] = useState(true)
+  const [open, setOpen] = useState(isRoot)
+  const [content, setContent] = useState({
+    title: data.title,
+    description: data.description,
+    image: data.image,
+    html: data.html
+  })
+
+  /*** Get contents from docs **/
+  const getContents = (content, element) => {
+    if (element && !element.id) {
+      if (!content.description && element.innerText) {
+        content.description = element.innerText
+      }
+      if (!content.image && element.getElementsByTagName('img') && element.getElementsByTagName('img')[0]) {
+        try {
+          const source = element.getElementsByTagName('img')[0].outerHTML.split('src="')[1].split('"')[0]
+          content.image = data.link + source
+        } catch (err) {
+
+        }
+      }
+      return [element.outerHTML].concat(getContents(content, element.nextElementSibling))
+    }
+    return []
+  }
+
+  useEffect(
+    () => {
+      setLoading(true)
+      fetch(data.link)
+        .then(response => response.text())
+        .then((response) => {
+          setLoading(false)
+
+          const parser = new DOMParser()
+          const htmlDoc = parser.parseFromString(response, 'text/html')
+          const _element = htmlDoc.getElementById(data.anchor.replace('#', ''))
+          if (!_element) return
+          if (!content.title) {
+            content.title = _element.innerText.replaceAll('¶', '')
+          }
+          const contents = getContents(content, _element.nextElementSibling)
+          if (contents.length) {
+            content.html = contents.join('').replaceAll(
+              'src="', `src="${data.link}`
+            )
+          }
+          setContent({ ...content })
+        })
+        .catch(err => {
+          setLoading(false)
+        })
+    }, [])
+
+  return <Fragment>
+    {/* BLOCK AS BUTTON */}
+    {
+      !isRoot ?
+        <div
+          tabIndex="-1"
+          className='section'
+          onClick={_ => {
+            if (!loading) {
+              setOpen(true)
+            }
+          }}
+        >
+          <div className='left'
+               style={!content.image ? { backgroundColor: "white" } : {}}>
+            {content.image ? <img src={content.image}/> : null}
+          </div>
+          {
+            !loading ?
+              <div className='right'>
+                <div className='title'>{content.title}</div>
+                <div className='content'>{content.description}</div>
+              </div> :
+              <div className='right'>
+                <div className='Throbber'>
+                  <CircularProgress/> Loading information...
+                </div>
+              </div>
+          }
+        </div> : null
+    }
+
+    {/* CONTENT */}
+    <div className={'HelpCenter-Block ' + (open ? 'Open' : '')}>
+      <div className='title'>
+        {
+          !isRoot ? <ArrowForwardIcon onClick={_ => {
+            setOpen(false)
+          }}/> : null
+        }
+        {content.title}
+      </div>
+      {
+        content.html ?
+          <div dangerouslySetInnerHTML={{ __html: content.html }}></div> :
+          null
+      }
+      {
+        data.blocks?.map((row, idx) => {
+          return <Block key={idx} data={row}/>
+        })
+      }
+      <a
+        tabIndex="-1"
+        href={data.link}
+        target={'_blank'}
+        className='link'>
+        Visit our Documentation <ArrowForwardIcon/>
+      </a>
+    </div>
+  </Fragment>
+}
--- django_project/frontend/src/components/HelpCenter/index.jsx
+++ django_project/frontend/src/components/HelpCenter/index.jsx
@@ -9,31 +9,38 @@
  *     (at your option) any later version.
  *
  * __author__ = 'irwan@kartoza.com'
- * __date__ = '08/08/2023'
+ * __date__ = '22/08/2023'
  * __copyright__ = ('Copyright 2023, Unicef')
  */
 
 import React, { forwardRef, useImperativeHandle, useState } from 'react';
-import doc_links, { root } from "../../app/doc_links";
-import {
-  ArrowForwardIcon,
-  ChatBubbleIcon,
-  CloseIcon,
-  EmailIcon
-} from "../Icons";
-import { ThemeButton } from "../Elements/Button";
+import { Button } from "@mui/material";
+
+import Block from "./Block";
+import { ChatBubbleIcon, CloseIcon, EmailIcon } from "../Icons";
+import data from './data.json';
 
 import './style.scss';
 
-
-/**
- * Help page section
+/** Themed button
+ * @param {string} buttonProps Variant of Button.
+ * @param {React.Component} children React component to be rendered
  */
+export function ThemeButton({ children, ...props }) {
+  return (
+    <Button {...props}
+            className={'ThemeButton ' + (props.className ? props.className : '')}>
+      {children}
+    </Button>
+  )
+}
 
-export const HelpPage = forwardRef(
+/** Help center section */
+
+export const HelpCenter = forwardRef(
   ({}, ref
   ) => {
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false)
 
     useImperativeHandle(ref, () => ({
       open() {
@@ -42,40 +49,27 @@ export const HelpPage = forwardRef(
     }));
 
     return <div
-      className={'HelpPageSection ' + (open ? 'Open' : '')}
+      className={'HelpCenter ' + (open ? 'Open' : '')}
       onClick={_ => {
         setOpen(false)
       }}>
-      <div className='HelpPageSection-Content' onClick={_ => {
+      <div className='HelpCenter-Content' onClick={_ => {
         _.stopPropagation();
       }}>
-        <div className='HelpPageSection-Close'>
+        <div className='HelpCenter-Close'>
           <CloseIcon
             onClick={_ => {
               setOpen(false)
             }}/>
         </div>
-        <div className='HelpPageSection-InnerContent'>
-          <div className='title'>GeoSight Help Center</div>
-          <a
-            tabIndex="-1"
-            href={doc_links.quickstart}
-            target={'_blank'}
-            className='section'>
-            <div className='title'>Get started with GeoSight.</div>
-            <div className='content'>
-              A quickstart to understanding geosight platform.
-            </div>
-          </a>
-          <a
-            tabIndex="-1"
-            href={root}
-            target={'_blank'}
-            className='link'>
-            Visit our Documentation <ArrowForwardIcon/>
-          </a>
+
+        {/* -------------------------------- */}
+        {/* CONTENT */}
+        <div className='HelpCenter-InnerContent'>
+          <Block data={data} isRoot={true}/>
         </div>
-        <div className='HelpPageSection-Footer'>
+        {/* -------------------------------- */}
+        <div className='HelpCenter-Footer'>
           <a
             tabIndex="-1"
             href='#'>
