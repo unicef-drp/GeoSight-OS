eb1431799adc1685acaa3d8069aaf4291e10ecd7
Irwan Fathurrahman <meomancer@gmail.com>
2023-08-28 14:21:56 +0700

    Update importer notification email

--- django_project/core/settings/prod.py
+++ django_project/core/settings/prod.py
@@ -35,8 +35,8 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp')
 EMAIL_PORT = ast.literal_eval(os.environ.get('EMAIL_PORT', '25'))
 # SMTP authentication information for EMAIL_HOST.
 # See fig.yml for where these are defined
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'noreply@kartoza.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'docker')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = ast.literal_eval(os.environ.get('EMAIL_USE_TLS', 'False'))
 EMAIL_USE_SSL = ast.literal_eval(os.environ.get('EMAIL_USE_SSL', 'False'))
 EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX', '')
