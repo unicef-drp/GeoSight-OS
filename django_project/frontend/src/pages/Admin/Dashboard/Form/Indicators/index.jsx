36eac54d361dfe2de020eadb3f7a6527dc2ab558
Irwan Fathurrahman <meomancer@gmail.com>
2023-09-12 16:44:59 +0700

    Update indicators on project as list (#67)

--- django_project/frontend/src/pages/Admin/Dashboard/Form/Indicators/index.jsx
+++ django_project/frontend/src/pages/Admin/Dashboard/Form/Indicators/index.jsx
@@ -1,23 +1,25 @@
 /**
-* GeoSight is UNICEF's geospatial web-based business intelligence platform.
-*
-* Contact : geosight-no-reply@unicef.org
-*
-* .. note:: This program is free software; you can redistribute it and/or modify
-*     it under the terms of the GNU Affero General Public License as published by
-*     the Free Software Foundation; either version 3 of the License, or
-*     (at your option) any later version.
-*
-* __author__ = 'irwan@kartoza.com'
-* __date__ = '13/06/2023'
-* __copyright__ = ('Copyright 2023, Unicef')
-*/
+ * GeoSight is UNICEF's geospatial web-based business intelligence platform.
+ *
+ * Contact : geosight-no-reply@unicef.org
+ *
+ * .. note:: This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation; either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ * __author__ = 'irwan@kartoza.com'
+ * __date__ = '13/06/2023'
+ * __copyright__ = ('Copyright 2023, Unicef')
+ */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { Actions } from "../../../../../store/dashboard";
 import { dictDeepCopy } from "../../../../../utils/main";
 import ListForm from '../ListForm'
+import { COLUMNS } from "../../../Components/List";
+import { DeleteButton } from "../../../../../components/Elements/Button";
 
 import './style.scss';
 
@@ -26,23 +28,33 @@ import './style.scss';
  */
 export default function IndicatorsForm() {
   const dispatch = useDispatch();
+  const [selectionModel, setSelectionModel] = useState([]);
   const { indicators } = dictDeepCopy(
     useSelector(state => state.dashboard.data)
   )
-  const indicatorList = indicators.map(indicator => {
-    indicator.name = indicator.full_name
-    return indicator
-  }).sort((a, b) => {
-    if (a.name < b.name) return -1
-    if (a.name > b.name) return 1
-    return 0
-  })
+  const columns = dictDeepCopy(
+    COLUMNS('Indicators', urls.admin.indicatorList)
+  )
+  delete columns[4]
+
+  // Delete Button
+  const deleteButton = () => {
+    return <DeleteButton
+      disabled={!selectionModel.length}
+      variant="Error Reverse"
+      text={"Delete"}
+      onClick={() => {
+        dispatch(Actions.Indicators.batchRemove(selectionModel))
+        setSelectionModel([])
+      }}
+    />
+  }
   return <ListForm
     pageName={'Indicators'}
-    data={indicatorList}
+    data={indicators}
     dataStructure={{
       group: '',
-      children: indicatorList.map(row => row.id)
+      children: indicators.map(row => row.id)
     }}
     setDataStructure={structure => {
     }}
@@ -56,6 +68,14 @@ export default function IndicatorsForm() {
     changeLayerAction={(layer) => {
       dispatch(Actions.Indicators.update(layer))
     }}
+    hasGroup={false}
     selectable={true}
+    listConfig={{
+      initData: indicators,
+      columns: columns,
+      selectionModel: selectionModel,
+      setSelectionModel: setSelectionModel,
+      header: deleteButton()
+    }}
   />
 }
