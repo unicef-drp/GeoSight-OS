e7a8f794dbea81bc629f8c58443c9b4de74f502c
Irwan Fathurrahman <meomancer@gmail.com>
2023-10-12 15:42:05 +0700

    Fix attributions

--- django_project/frontend/src/store/dashboard/reducers/globalState/index.jsx
+++ django_project/frontend/src/store/dashboard/reducers/globalState/index.jsx
@@ -1,17 +1,17 @@
 /**
-* GeoSight is UNICEF's geospatial web-based business intelligence platform.
-*
-* Contact : geosight-no-reply@unicef.org
-*
-* .. note:: This program is free software; you can redistribute it and/or modify
-*     it under the terms of the GNU Affero General Public License as published by
-*     the Free Software Foundation; either version 3 of the License, or
-*     (at your option) any later version.
-*
-* __author__ = 'irwan@kartoza.com'
-* __date__ = '13/06/2023'
-* __copyright__ = ('Copyright 2023, Unicef')
-*/
+ * GeoSight is UNICEF's geospatial web-based business intelligence platform.
+ *
+ * Contact : geosight-no-reply@unicef.org
+ *
+ * .. note:: This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation; either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ * __author__ = 'irwan@kartoza.com'
+ * __date__ = '13/06/2023'
+ * __copyright__ = ('Copyright 2023, Unicef')
+ */
 
 /**
  * GLOBAL_STATE reducer
@@ -23,6 +23,7 @@ export const GLOBAL_STATE_ACTION_TYPE_UPDATE = 'GLOBAL_STATE/UPDATE';
 const initialState = {
   globalDateSelectorOpened: false,
   showIndicatorMapLabel: true,
+  attributions: []
 }
 export default function globalStateReducer(state = initialState, action) {
   if (action.name === GLOBAL_STATE_ACTION_NAME) {
