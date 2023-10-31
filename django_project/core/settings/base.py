8dfb41d45c03c17ceabe87774a995dbe5e4fdbd6
Irwan Fathurrahman <meomancer@gmail.com>
2023-10-31 14:21:45 +0700

    Increase page size

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': (
         'rest_framework.pagination.LimitOffsetPagination'
     ),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 LOGIN_URL = '/login/'
 SITE_ID = 1
