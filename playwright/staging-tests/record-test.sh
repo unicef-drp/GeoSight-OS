df11f1e786307f70e4eff81b6333c6a82f7e0205
Tim Sutton <tim@kartoza.com>
2023-11-01 22:36:38 +0000

    Record tests in typescript rather

--- playwright/staging-tests/record-test.sh
+++ playwright/staging-tests/record-test.sh
@@ -4,21 +4,21 @@ if [ -z "$1" ]
 then
   echo "Usage: $0 TESTNAME"
   echo "e.g. $0 mytest"
-  echo "will write a new test to tests/mytest.py"
+  echo "will write a new test to tests/mytest.ts"
   echo "Do not use spaces in your test name."
   echo ""
   echo "After recording your test, close the test browser."
   echo "You can then run your test by doing:"
-  echo "pytest tests/mytest.py"
+  echo "pytest tests/mytest.ts"
   exit
 else
   echo "Recording test to tests\$1"
 fi
 
-if [ -w "tests/${1}.py" ]; then
+if [ -w "tests/${1}.ts" ]; then
    # File exists and write permission granted to user
    # show prompt
-   echo "File tests/${1}.py exists. Overwrite? y/n"
+   echo "File tests/${1}.ts exists. Overwrite? y/n"
    read ANSWER
    case $ANSWER in 
        [yY] ) echo "Writing recorded test to tests/${1}.py" ;;
@@ -29,11 +29,11 @@ TESTNAME=$1
 
 playwright \
   codegen \
-  --target python \
+  --target playwright-test \
   --load-storage=geosight-auth.json \
-  -o tests/$TESTNAME.py \
+  -o tests/$TESTNAME.ts \
   https://staging-geosight.unitst.org
 
 echo "Test recording completed."
 echo "You can then run your test by doing:"
-echo "pytest tests/$1.py"
+echo "playwright test --project chromium tests/$1.ts"
