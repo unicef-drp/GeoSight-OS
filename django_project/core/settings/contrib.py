3744a73682ebd4b9270b52c1662c9388deb654bc
Irwan Fathurrahman <meomancer@gmail.com>
2023-12-04 10:06:09 +0700

    Make geosight api not have expires

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -56,3 +56,13 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+# knox setting
+REST_KNOX = {
+    'SECURE_HASH_ALGORITHM': 'cryptography.hazmat.primitives.hashes.SHA512',
+    'AUTH_TOKEN_CHARACTER_LENGTH': 64,
+    'TOKEN_TTL': None,
+    'USER_SERIALIZER': 'knox.serializers.UserSerializer',
+    'TOKEN_LIMIT_PER_USER': 1,
+    'AUTO_REFRESH': False,
+}
