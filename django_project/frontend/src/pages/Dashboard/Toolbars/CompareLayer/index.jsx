9719843c1ef2a9bec5f7bcbce274e85412e7a56e
Irwan Fathurrahman <meomancer@gmail.com>
2024-02-01 11:56:37 +0700

    Update icons (#199)

--- django_project/frontend/src/pages/Dashboard/Toolbars/CompareLayer/index.jsx
+++ django_project/frontend/src/pages/Dashboard/Toolbars/CompareLayer/index.jsx
@@ -1,17 +1,17 @@
 /**
-* GeoSight is UNICEF's geospatial web-based business intelligence platform.
-*
-* Contact : geosight-no-reply@unicef.org
-*
-* .. note:: This program is free software; you can redistribute it and/or modify
-*     it under the terms of the GNU Affero General Public License as published by
-*     the Free Software Foundation; either version 3 of the License, or
-*     (at your option) any later version.
-*
-* __author__ = 'irwan@kartoza.com'
-* __date__ = '13/06/2023'
-* __copyright__ = ('Copyright 2023, Unicef')
-*/
+ * GeoSight is UNICEF's geospatial web-based business intelligence platform.
+ *
+ * Contact : geosight-no-reply@unicef.org
+ *
+ * .. note:: This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation; either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ * __author__ = 'irwan@kartoza.com'
+ * __date__ = '13/06/2023'
+ * __copyright__ = ('Copyright 2023, Unicef')
+ */
 
 /* ==========================================================================
    CompareLayer
@@ -19,9 +19,12 @@
 
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import CompareIcon from '@mui/icons-material/Compare';
 import { Plugin, PluginChild } from "../../MapLibre/Plugin";
 import { Actions } from '../../../../store/dashboard'
+import {
+  CompareCheckedIcon,
+  CompareUncheckedIcon
+} from "../../../../components/Icons";
 
 import './style.scss';
 
@@ -45,17 +48,19 @@ export default function CompareLayer({ disabled = false }) {
 
   return (
     <Plugin>
-      <div
-        className={'CompareLayerComponent ' + (compareMode ? "Active" : "Inactive")}>
+      <div className='CompareLayerComponent Active'>
         <PluginChild
           title={(compareMode ? 'Turn off' : 'Turn on') + ' compare Layers'}
           disabled={disabled}
-        >
-          <CompareIcon onClick={() => {
+          onClick={() => {
             if (!disabled) {
               dispatch(Actions.MapMode.changeCompareMode())
             }
-          }}/>
+          }}
+        >
+          {
+            compareMode ? <CompareCheckedIcon/> : <CompareUncheckedIcon/>
+          }
         </PluginChild>
       </div>
     </Plugin>
