4d3a99fc481b0fcb9996e422bd0a8d2e690170bf
Irwan Fathurrahman <meomancer@gmail.com>
2024-04-27 13:13:30 +0700

    Fix tests

--- django_project/frontend/tests/admin/reference_dataset.py
+++ django_project/frontend/tests/admin/reference_dataset.py
@@ -55,8 +55,8 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         url = reverse(self.list_url_tag)
         self.assertRequestGetView(url, 302)  # Non login
         self.assertRequestGetView(url, 403, self.viewer)  # Viewer
-        self.assertRequestGetView(url, 200, self.contributor)  # Contributor
-        self.assertRequestGetView(url, 200, self.creator)  # Creator
+        self.assertRequestGetView(url, 403, self.contributor)  # Contributor
+        self.assertRequestGetView(url, 403, self.creator)  # Creator
         self.assertRequestGetView(url, 200, self.admin)  # Admin
 
     def test_create_view(self):
@@ -65,7 +65,7 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.assertRequestGetView(url, 302)  # Non login
         self.assertRequestGetView(url, 403, self.viewer)  # Viewer
         self.assertRequestGetView(url, 403, self.contributor)  # Contributor
-        self.assertRequestGetView(url, 200, self.creator)  # Creator
+        self.assertRequestGetView(url, 403, self.creator)  # Creator
         self.assertRequestGetView(url, 200, self.admin)  # Admin
 
         # POST it
@@ -76,10 +76,11 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.assertRequestPostView(url, 403, new_payload, self.viewer)
         self.assertRequestPostView(url, 403, new_payload, self.contributor)
 
-        self.assertRequestPostView(url, 302, new_payload, self.creator)
+        self.assertRequestPostView(url, 403, new_payload, self.creator)
+        self.assertRequestPostView(url, 302, new_payload, self.admin)
         new_resource = self.get_resources(self.creator).last()
         self.assertEqual(new_resource.name, new_payload['name'])
-        self.assertEqual(new_resource.creator, self.creator)
+        self.assertEqual(new_resource.creator, self.admin)
 
         # Check the edit permission
         url = reverse(
@@ -88,7 +89,7 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.assertRequestGetView(url, 302)  # Non login
         self.assertRequestGetView(url, 403, self.viewer)  # Viewer
         self.assertRequestGetView(url, 403, self.contributor)  # Contributor
-        self.assertRequestGetView(url, 200, self.creator)  # Creator
+        self.assertRequestGetView(url, 403, self.creator)  # Creator
         self.assertRequestGetView(url, 403, self.resource_creator)  # Creator
         self.assertRequestGetView(url, 200, self.admin)  # Admin
 
@@ -107,7 +108,7 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.assertRequestGetView(url, 403, self.viewer)  # Viewer
         self.assertRequestGetView(url, 403, self.contributor)  # Contributor
         self.assertRequestGetView(url, 403, self.creator)  # Creator
-        self.assertRequestGetView(url, 200, self.resource_creator)  # Creator
+        self.assertRequestGetView(url, 403, self.resource_creator)  # Creator
         self.assertRequestGetView(url, 200, self.admin)  # Admin
 
         # sharing
@@ -121,7 +122,7 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.assertRequestGetView(url, 403, self.creator)  # Creator
         self.permission.update_user_permission(
             self.creator, PERMISSIONS.WRITE.name)
-        self.assertRequestGetView(url, 200, self.creator)  # Creator
+        self.assertRequestGetView(url, 403, self.creator)  # Creator
 
         self.permission.update_group_permission(
             self.group, PERMISSIONS.READ.name)
@@ -132,8 +133,8 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.permission.update_group_permission(
             self.group, PERMISSIONS.WRITE.name)
         self.assertRequestGetView(url, 403, self.viewer_in_group)
-        self.assertRequestGetView(url, 200, self.contributor_in_group)
-        self.assertRequestGetView(url, 200, self.creator_in_group)
+        self.assertRequestGetView(url, 403, self.contributor_in_group)
+        self.assertRequestGetView(url, 403, self.creator_in_group)
 
         # POST it
         new_payload = copy.deepcopy(self.payload)
@@ -142,6 +143,9 @@ class ReferenceDatasetViewTest(BasePermissionTest, TestCase):
         self.assertRequestPostView(url, 403, new_payload, self.viewer)
         self.assertRequestPostView(url, 403, new_payload, self.contributor)
 
+        self.assertRequestPostView(url, 403, new_payload, self.creator)
+        self.creator.profile.manage_local_dataset = True
+        self.creator.profile.save()
         self.assertRequestPostView(url, 302, new_payload, self.creator)
         self.resource.refresh_from_db()
         self.assertEqual(self.resource.name, new_payload['name'])
