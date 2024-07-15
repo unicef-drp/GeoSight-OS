e88912d7a50a4eeac86cb1d113ab37418158f7d6
Irwan Fathurrahman <meomancer@gmail.com>
2024-07-15 12:39:57 +0700

    Add related name for creator

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -99,6 +99,7 @@ TEMPLATES = [
         'DIRS': [
             # Put Templates Hers
             ABS_PATH('core', 'templates'),
+            ABS_PATH('frontend', 'templates'),
         ],
         'OPTIONS': {
             'loaders': [
