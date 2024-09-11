dedc3d188235fc3d36fd8605154b3dc520d0eafc
Irwan Fathurrahman <meomancer@gmail.com>
2024-09-11 07:35:57 +0700

    Fix flake

--- django_project/geosight/importer/models/log.py
+++ django_project/geosight/importer/models/log.py
@@ -22,9 +22,6 @@ from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from core.models.preferences import SitePreferences
-from geosight.data.models.indicator.indicator_value import (
-    IndicatorValueWithGeo
-)
 from geosight.importer.models.importer import Importer
 
 
