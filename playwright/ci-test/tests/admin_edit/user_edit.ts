a353c6823d76d3537192132c5a26875cebc79252
Irwan Fathurrahman <meomancer@gmail.com>
2025-01-21 11:24:46 +0700

    Fix user form that show Role for non admin

---
+++ playwright/ci-test/tests/admin_edit/user_edit.ts
@@ -0,0 +1,48 @@
+import { expect, test } from '@playwright/test';
+
+const timeout = 2000;
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const _url = '/admin/user/admin/edit#General'
+
+test.describe('Users edit profile', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(_url);
+  });
+
+  test('Test edit functions', async ({ page }) => {
+    await page.locator('.ReactSelect__input-container').click();
+    await page.getByRole('option', { name: 'Super Admin' }).click();
+    await page.locator('#Form #id_first_name').fill('Admin 1');
+    await page.locator('#Form #id_last_name').fill('Admin 2');
+    await page.getByRole('button', { name: 'Save' }).click();
+    await expect(page.getByRole('grid')).toContainText('Admin 1');
+    await expect(page.getByRole('grid')).toContainText('Admin 2');
+
+    await page.goto(_url)
+    await page.getByText('API Key', { exact: true }).click();
+    await page.getByRole('textbox').fill('test');
+    await page.getByRole('button', { name: 'Generate API Key' }).click();
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await expect(page.getByText('Platform : test')).toBeVisible();
+    await page.getByRole('button', { name: 'Delete Api Key' }).click();
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await expect(page.getByText('Platform : test')).toBeHidden();
+  });
+
+  test('Test edit functions for non admin', async ({ page }) => {
+    await page.getByText('a', { exact: true }).nth(1).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#app input[name="username"]').fill('contributor');
+    await page.locator('#app input[name="password"]').fill('contributor');
+    await page.getByRole('button', { name: 'LOG IN' }).click();
+    await page.getByText('c', { exact: true }).click();
+    await page.getByRole('link', { name: 'Profile' }).click();
+    await page.locator('#Form #id_first_name').fill('Name');
+    await page.locator('#Form #id_last_name').fill('Last');
+    await page.locator('#Form #id_email').fill('contributor@gmail.com');
+    await expect(page.locator('.ReactSelect__input-container .ReactSelect__input')).toBeDisabled();
+    await expect(page.locator('#Form #id_email')).toHaveValue('contributor@gmail.com');
+  });
+})
