bcbbf952ce4d80002f447cbcbf1653acbf2b53a2
zamuzakki <zakki@kartoza.com>
2025-01-22 16:51:24 +0700

    Restyle Raster layout

--- django_project/frontend/src/pages/Admin/ContextLayer/StyleConfig/RasterCogLayer/index.jsx
+++ django_project/frontend/src/pages/Admin/ContextLayer/StyleConfig/RasterCogLayer/index.jsx
@@ -16,6 +16,7 @@
 import React, {Fragment, useEffect, useState, useMemo} from 'react';
 import { debounce } from '@mui/material/utils';
 import {FormControl} from "@mui/material";
+import Grid from '@mui/material/Grid';
 import ColorPaletteStyleConfig, {QUANTITATIVE_TYPE} from "../../../Style/Form/DynamicStyleConfig/Palette";
 import {dictDeepCopy} from "../../../../../utils/main";
 import {dynamicClassificationChoices} from "../../../Style/Form/DynamicStyleConfig";
@@ -137,62 +138,75 @@ export default function RasterCogLayer(
       />
       <br/>
       <br/>
-      <div>
-        <div className="BasicFormSection">
-          <div className='RuleTable-Title'>NoData Value</div>
-          <input
-            placeholder='NoData Value' type='text'
-            value={styles.nodata}
-            disabled={true}
-            style={{width: "fit-content", flexGrow: 1}}
-          />
-        </div>
-        <div className="BasicFormSection">
-          <div className='RuleTable-Title'>Additional NoData Value</div>
-          <input
-            placeholder='Additional NoData Value' type='number'
-            value={styles.additional_nodata}
-            onChange={evt => {
-              setData({
-                ...data,
-                styles: {
-                  ...styles,
-                  additional_nodata: evt.target.value
-                }
-              })
-            }}
-            style={{width: "fit-content", flexGrow: 1}}
-          />
-        </div>
-        <div className="BasicFormSection">
-          <div className='RuleTable-Title'>NoData Color</div>
-          <ColorSelector
-            color={noDataColor}
-            onChange={evt => {
-              setNoDataColor(evt.target.value)
-            }}
-            hideInput={true}
-            fullWidth={true}
-          />
-        </div>
-        <div className="BasicFormSection">
-          <div className='RuleTable-Title'>NoData Opacity (0-100)</div>
-          <input
-            placeholder='NoData Opacity' type='number'
-            value={styles.nodata_opacity}
-            onChange={evt => {
-              setData({
-                ...data,
-                styles: {
-                  ...styles,
-                  nodata_opacity: parseFloat(evt.target.value)
-                }
-              })
-            }}
-            style={{width: "fit-content", flexGrow: 1}}
-          />
-        </div>
-      </div>
+      <Grid
+        container
+        flexDirection={'row'}
+        spacing={1}
+      >
+        <Grid item md={12} xl={12} lg={12}>
+          NoData Configuration
+        </Grid>
+        <Grid item md={3} xl={3} lg={3}>
+          <div>
+            <div>Original NoData</div>
+            <input
+              placeholder='Original NoData' type='text'
+              value={styles.nodata}
+              disabled={true}
+            />
+          </div>
+        </Grid>
+        <Grid item md={3} xl={3} lg={3}>
+          <div>
+            <div>Additional NoData</div>
+            <input
+              placeholder='Additional NoData' type='number'
+              value={styles.additional_nodata}
+              onChange={evt => {
+                setData({
+                  ...data,
+                  styles: {
+                    ...styles,
+                    additional_nodata: evt.target.value
+                  }
+                })
+              }}
+            />
+          </div>
+        </Grid>
+        <Grid item md={3} xl={3} lg={3}>
+          <div>
+            <div>Color</div>
+            <ColorSelector
+              color={noDataColor}
+              onChange={evt => {
+                setNoDataColor(evt.target.value)
+              }}
+              hideInput={true}
+              fullWidth={true}
+            />
+          </div>
+        </Grid>
+        <Grid item md={3} xl={3} lg={3}>
+          <div>
+            <div>Opacity (0-100)</div>
+            <input
+              placeholder='Opacity' type='number'
+              value={styles.nodata_opacity}
+              onChange={evt => {
+                setData({
+                  ...data,
+                  styles: {
+                    ...styles,
+                    nodata_opacity: parseFloat(evt.target.value)
+                  }
+                })
+              }}
+            />
+          </div>
+        </Grid>
+      </Grid>
+      <br/>
       {/*  Palette */}
       <ColorPaletteStyleConfig
         styleType={QUANTITATIVE_TYPE}
