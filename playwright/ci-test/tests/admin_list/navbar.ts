39c11f19668ce21dbe82cb1e4f6bed80a3fc780b
zamuzakki <zakki@kartoza.com>
2025-01-23 20:00:52 +0700

    Fix E2E for testing navheader link

--- playwright/ci-test/tests/admin_list/navbar.ts
+++ playwright/ci-test/tests/admin_list/navbar.ts
@@ -5,6 +5,8 @@ test.describe('Check Admin Navbar', () => {
   test('Check Navbar Production', async ({ page }) => {
     await page.goto('/django-admin/core/sitepreferences/1/change/');
     await page.getByLabel('Site type:').selectOption('Production');
+    await page.getByLabel('Georepo api key level 1:').fill('aa');
+    await page.getByLabel('Georepo api key level 4:').fill('aa');
     await page.getByRole('button', { name: 'Save and continue editing' }).click();
 
     await page.goto('/admin/indicators/');
@@ -27,12 +29,16 @@ test.describe('Check Admin Navbar', () => {
   test('Check Navbar Staging', async ({ page }) => {
     await page.goto('/django-admin/core/sitepreferences/1/change/');
     await page.getByLabel('Site type:').selectOption('Staging');
+    await page.getByLabel('Georepo api key level 1:').fill('aa');
+    await page.getByLabel('Georepo api key level 4:').fill('aa');
     await page.getByRole('button', { name: 'Save and continue editing' }).click();
 
     await page.goto('/admin/indicators/');
     // Check initial state
     const headerLink = page.locator('a.NavHeaderLink');
-    await expect(headerLink).toHaveText('GeoSight')
+    await expect(headerLink).toHaveText('GeoSight Staging')
+    await expect(page.locator('a.NavHeaderLink span')).toHaveCSS('display', 'none');
+    await expect(page.locator('a.NavHeaderLink span')).toHaveText('Staging');
 
     const navBar = page.locator('.Nav-Staging');
     // Check the CSS property
--- playwright/ci-test/tests/project_view/navbar.ts
+++ playwright/ci-test/tests/project_view/navbar.ts
@@ -5,6 +5,8 @@ test.describe('Check Project View Navbar', () => {
   test('Check Navbar Production', async ({ page }) => {
     await page.goto('/django-admin/core/sitepreferences/1/change/');
     await page.getByLabel('Site type:').selectOption('Production');
+    await page.getByLabel('Georepo api key level 1:').fill('aa');
+    await page.getByLabel('Georepo api key level 4:').fill('aa');
     await page.getByRole('button', { name: 'Save and continue editing' }).click();
 
     await page.goto('/project/demo-geosight-project');
@@ -14,20 +16,15 @@ test.describe('Check Project View Navbar', () => {
     const headerLink = page.locator('a.NavHeaderLink');
     await expect(headerLink).toHaveText('GeoSight')
 
-    const navBar = page.locator('.Nav-Production');
-    // Check the CSS property
-    const backgroundColor = await navBar.evaluate((element) =>
-      window.getComputedStyle(element).getPropertyValue('background-color')
-    );
-
-    // Assert the background color (adjust the expected value)
-    expect(backgroundColor).toBe('rgb(28, 171, 226)');
+    await expect(page.locator('.Nav-Production')).toHaveCSS("background-color", 'rgb(28, 171, 226)');
   });
 
     // A use case tests scenarios
   test('Check Navbar Staging', async ({ page }) => {
     await page.goto('/django-admin/core/sitepreferences/1/change/');
     await page.getByLabel('Site type:').selectOption('Staging');
+    await page.getByLabel('Georepo api key level 1:').fill('aa');
+    await page.getByLabel('Georepo api key level 4:').fill('aa');
     await page.getByRole('button', { name: 'Save and continue editing' }).click();
 
     await page.goto('/project/demo-geosight-project');
@@ -36,14 +33,7 @@ test.describe('Check Project View Navbar', () => {
     const headerLink = page.locator('a.NavHeaderLink');
     await expect(headerLink).toHaveText('GeoSight Staging')
 
-    const navBar = page.locator('.Nav-Staging');
-    // Check the CSS property
-    const backgroundColor = await navBar.evaluate((element) =>
-      window.getComputedStyle(element).getPropertyValue('background-color')
-    );
-
-    // Assert the background color (adjust the expected value)
-    expect(backgroundColor).toBe('rgb(255, 0, 0)');
+    await expect(page.locator('.Nav-Staging')).toHaveCSS("background-color", 'rgb(255, 0, 0)');
   });
 
 });
