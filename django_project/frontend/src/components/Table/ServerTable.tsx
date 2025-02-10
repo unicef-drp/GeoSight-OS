5032a6352217427be2660172c21aba595e163d15
Irwan Fathurrahman <meomancer@gmail.com>
2025-02-10 12:21:12 +0700

    Fix dataset trigger filters

--- django_project/frontend/src/components/Table/ServerTable.tsx
+++ django_project/frontend/src/components/Table/ServerTable.tsx
@@ -168,8 +168,9 @@ const ServerTable = forwardRef(
 
     /*** Load data */
     const loadData = (force: boolean) => {
-      let _parameters = dictDeepCopy(
-        getParameters ? getParameters(parameters) : parameters
+      let _parameters = dictDeepCopy(parameters)
+      _parameters = dictDeepCopy(
+        getParameters ? getParameters(_parameters) : _parameters
       )
       _parameters.page += 1
 
@@ -257,6 +258,7 @@ const ServerTable = forwardRef(
         setParameters({ ...parameters, sort: getSort(sortModel) })
       }, [sortModel]
     )
+
 
     return (
       <Fragment>
