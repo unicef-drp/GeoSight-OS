5c251ab6736ad99d7fdd4797da2851d1f1a2b3cd
zamuzakki <zakki@kartoza.com>
2025-02-17 13:47:41 +0700

    Add test for scheduled jobs

--- django_project/geosight/importer/api/scheduled_job.py
+++ django_project/geosight/importer/api/scheduled_job.py
@@ -20,13 +20,13 @@ from django.http import HttpResponseBadRequest
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
-from core.api.base import FilteredAPI
 from core.pagination import Pagination
+from geosight.data.api.v1.base import BaseApiV1
 from geosight.importer.models import Importer
 from geosight.importer.serializer.importer import ImporterSerializer
 
 
-class ScheduleJobListAPI(ListAPIView, FilteredAPI):
+class ScheduleJobListAPI(ListAPIView, BaseApiV1):
     """Return Schedule Job list."""
 
     pagination_class = Pagination
@@ -34,14 +34,21 @@ class ScheduleJobListAPI(ListAPIView, FilteredAPI):
 
     def get_queryset(self):
         """Return queryset of API."""
-        query = Importer.objects.all().order_by('job_name')
+        query = Importer.objects.all()
+        if self.request.user.is_anonymous:
+            return query.none()
         if not self.request.user.profile.is_admin:
             query = Importer.objects.filter(
                 creator=self.request.user
             ).order_by('job_name')
 
         # Filter by parameters
-        query = self.filter_query(self.request, query, ['page', 'page_size'])
+        query = self.filter_query(
+            request=self.request,
+            query=query,
+            ignores=['page', 'page_size'],
+            sort=self.request.query_params.get('sort')
+        )
         return query
 
     def put(self, request):
@@ -62,7 +69,7 @@ class ScheduleJobListAPI(ListAPIView, FilteredAPI):
         return Response('Deleted')
 
     def delete(self, request):
-        """Delete an basemap."""
+        """Delete an importer."""
         ids = json.loads(request.data['ids'])
         for obj in Importer.objects.filter(id__in=ids):
             if obj.able_to_edit(self.request.user):
