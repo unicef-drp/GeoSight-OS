a46171e3c7f93a3857e9adda4fb7ff181ba7891f
Irwan Fathurrahman <meomancer@gmail.com>
2025-05-22 12:16:51 +0700

    Rename run-vscode.sh to vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -7,7 +7,7 @@ if ! command -v code &> /dev/null; then
     echo "❌ 'code' CLI not found. Please install VS Code and add 'code' to your PATH. See the documentation https://unicef-drp.github.io/GeoSight-OS-Documentation/developer/setup/run-with-vscode/#prerequisites"
     exit 1
 fi
-    
+
 # Check if the extension is already installed in the custom dir
 if code --list-extensions | grep -q "$EXTENSION_ID"; then
     echo "✅ '$EXTENSION_ID' is already installed in '$EXT_DIR'."
@@ -25,5 +25,3 @@ fi
 
 echo "🚀 Opening VS Code in current folder"
 code .
-
-
