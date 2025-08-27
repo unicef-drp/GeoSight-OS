73c12e97e4bf67e60efde7a701799c5be6ffc72e
Irwan Fathurrahman <meomancer@gmail.com>
2025-08-27 13:11:16 +0700

    Fix regression: No delete button on dashbord form (#534)
    * Fix regression: No delete button on dashbord form
    
    * Fix flake

--- django_project/core/cache.py
+++ django_project/core/cache.py
@@ -21,22 +21,40 @@ logger = logging.getLogger(__name__)
 
 
 class VersionCache:
-    """Geosight cache system with version."""
+    """
+    Geosight cache system with version control.
 
-    def __init__(self, key, version):
-        """Initiate data."""
+    This class provides a caching mechanism that associates a cache entry with
+    a version key.
+    If the version key in the cache matches the expected version,
+    the cached data is returned; otherwise, the cache is considered invalid.
+    """
+
+    def __init__(self, key: str, version: str, request=None):  # noqa DOC101
+        if request and request.user.is_authenticated:
+            key += f"-{request.user.id}"
         self.key = key
         self.version_key = f'{key}-version'
         self.version = version
 
     def get(self):
-        """Return data from cache."""
+        """
+        Retrieve data from cache if the version matches.
+
+        :return: Cached data if the version matches, otherwise ``None``.
+        :rtype: dict or None
+        """
         if cache.get(self.version_key) == self.version:
             return cache.get(self.key)
         return None
 
     def set(self, data: dict):
-        """Set data to cache."""
+        """
+        Store data in the cache along with its version.
+
+        :param data: Data to be cached.
+        :type data: dict
+        """
         try:
             cache.set(self.key, data)
             cache.set(self.version_key, self.version)
