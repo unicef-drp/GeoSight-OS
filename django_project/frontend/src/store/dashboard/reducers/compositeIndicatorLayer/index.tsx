b0d51ec0601af2f3adf84c23f6da22c06cb7dc5a
Irwan Fathurrahman <meomancer@gmail.com>
2025-09-11 12:47:13 +0700

    Remove console.log

--- django_project/frontend/src/store/dashboard/reducers/compositeIndicatorLayer/index.tsx
+++ django_project/frontend/src/store/dashboard/reducers/compositeIndicatorLayer/index.tsx
@@ -74,7 +74,6 @@ export default function compositeIndexLayerReducer(
             });
           }
         });
-        console.log(state.data);
         return {
           ...state,
           data: { ...state.data },
