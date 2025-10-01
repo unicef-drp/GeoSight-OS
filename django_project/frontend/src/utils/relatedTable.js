9a7190202a9de8c99647a7170f600b3feefb1201
Irwan Fathurrahman <meomancer@gmail.com>
2025-10-01 10:46:02 +0700

    Fix query of RT (#541)

--- django_project/frontend/src/utils/relatedTable.js
+++ django_project/frontend/src/utils/relatedTable.js
@@ -14,111 +14,172 @@
  */
 
 import alasql from "alasql";
-import { isValidDate, parseDateTime, updateDate } from "./main"
+import { isValidDate, parseDateTime, updateDate } from "./main";
 import { queryData, spacedField } from "./queryExtraction";
 import { COUNT_UNIQUE } from "../components/SqlQueryGenerator/Aggregation";
 import {
   INTERNEXT_QUERY_BEFORE,
   INTERNEXT_REGEX,
   INTERVAL_QUERY_BEFORE,
-  INTERVAL_REGEX
+  INTERVAL_REGEX,
 } from "../components/SqlQueryGenerator/WhereQueryGenerator";
 import { deepClone } from "@mui/x-data-grid/utils/utils";
 
-const cache = {}
+const cache = {};
+
+/**
+ * Clean where query
+ * @param where
+ * @returns {*}
+ */
+export const cleanWhereQuery = (where) => {
+  return where
+    .replaceAll('"', "`")
+    .replace(/`(.*?)`/g, function (match, text, href) {
+      if (match.includes("'")) {
+        return match.replaceAll("`", '"');
+      }
+      return match;
+    })
+    .replaceAll("value", "_value");
+};
+
+/**
+ * Update where query
+ * @param where
+ * @param config
+ * @returns {*}
+ */
+export const updateWhereQuery = (where, config) => {
+  // INTERVAL
+  const intervalRegex = INTERVAL_REGEX;
+  where = where
+    .replaceAll('"', "`")
+    .replace(/`(.*?)`/g, function (match, text, href) {
+      if (match.includes("'")) {
+        return match.replaceAll("`", '"');
+      }
+      return match;
+    })
+    .replace(intervalRegex, function (match, text, href) {
+      const clean = match
+        .replace(INTERVAL_QUERY_BEFORE, "")
+        .replaceAll("'", "")
+        .split(" ");
+      const amount = clean[0];
+      const interval = clean[1];
+      const date = new Date();
+      const now = Date.UTC(
+        date.getUTCFullYear(),
+        date.getUTCMonth(),
+        date.getUTCDate(),
+        date.getUTCHours(),
+        date.getUTCMinutes(),
+        date.getUTCSeconds(),
+      );
+      return `BETWEEN '${updateDate(new Date(now), -1 * parseInt(amount), interval).toISOString()}' AND '${new Date(now).toISOString()}'`;
+    });
+  // INTERNEXT
+  {
+    const intervalRegex = INTERNEXT_REGEX;
+    where = where
+      .replaceAll('"', "`")
+      .replace(/`(.*?)`/g, function (match, text, href) {
+        if (match.includes("'")) {
+          return match.replaceAll("`", '"');
+        }
+        return match;
+      })
+      .replace(intervalRegex, function (match, text, href) {
+        const clean = match
+          .replace(INTERNEXT_QUERY_BEFORE, "")
+          .replaceAll("'", "")
+          .split(" ");
+        const amount = clean[0];
+        const interval = clean[1];
+        const date = new Date();
+        const now = Date.UTC(
+          date.getUTCFullYear(),
+          date.getUTCMonth(),
+          date.getUTCDate(),
+          date.getUTCHours(),
+          date.getUTCMinutes(),
+          date.getUTCSeconds(),
+        );
+        return `BETWEEN '${new Date(now).toISOString()}' AND '${updateDate(new Date(now), parseInt(amount), interval).toISOString()}'`;
+      });
+  }
+  where = where
+    .replaceAll("< BETWEEN", " BETWEEN")
+    .replaceAll("> BETWEEN", " BETWEEN")
+    .replaceAll("<= BETWEEN", " BETWEEN")
+    .replaceAll(">= BETWEEN", " BETWEEN");
+  return where;
+};
+
 /**
  * Return related table data
  */
-export const getRelatedTableData = (data, config, selectedGlobalTime, geoField = 'geometry_code', aggregateDate = true, adminLevels = null) => {
+export const getRelatedTableData = (
+  data,
+  config,
+  selectedGlobalTime,
+  geoField = "geometry_code",
+  aggregateDate = true,
+  adminLevels = null,
+) => {
   if (data) {
     // Get admin level
     if (adminLevels == null) {
-      adminLevels = data[0]?.admin_level
+      adminLevels = data[0]?.admin_level;
     }
-    const identifier = JSON.stringify(
-      {
-        count: data.length,
-        config: config,
-        selectedGlobalTime: selectedGlobalTime,
-        geoField: geoField,
-        aggregateDate: aggregateDate,
-        adminLevels: adminLevels
-      }
-    )
+    const identifier = JSON.stringify({
+      count: data.length,
+      config: config,
+      selectedGlobalTime: selectedGlobalTime,
+      geoField: geoField,
+      aggregateDate: aggregateDate,
+      adminLevels: adminLevels,
+    });
 
     // Filter by admin level
     if (adminLevels !== null) {
-      data = data.filter(row => ('' + adminLevels).split(',').includes('' + row.admin_level))
+      data = data.filter((row) =>
+        ("" + adminLevels).split(",").includes("" + row.admin_level),
+      );
     }
-    data = JSON.parse(JSON.stringify(data))
-    const { aggregation } = config
+    data = JSON.parse(JSON.stringify(data));
+    const { aggregation } = config;
 
     // INTERVAL
-    let where = config?.where
-    {
-      const intervalRegex = INTERVAL_REGEX;
-      where = where.replaceAll('"', '`')
-        .replace(/`(.*?)`/g, function (match, text, href) {
-          if (match.includes("'")) {
-            return match.replaceAll('`', '"')
-          }
-          return match
-        })
-        .replace(intervalRegex, function (match, text, href) {
-          const clean = match.replace(INTERVAL_QUERY_BEFORE, "").replaceAll("'", "").split(' ')
-          const amount = clean[0]
-          const interval = clean[1]
-          const date = new Date()
-          const now = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
-            date.getUTCDate(), date.getUTCHours(),
-            date.getUTCMinutes(), date.getUTCSeconds());
-          return `BETWEEN '${updateDate(new Date(now), -1 * parseInt(amount), interval).toISOString()}' AND '${new Date(now).toISOString()}'`
-        });
-    }
+    let where = updateWhereQuery(config?.where);
 
-    // INTERNEXT
-    {
-      const intervalRegex = INTERNEXT_REGEX;
-      where = where.replaceAll('"', '`')
-        .replace(/`(.*?)`/g, function (match, text, href) {
-          if (match.includes("'")) {
-            return match.replaceAll('`', '"')
-          }
-          return match
-        })
-        .replace(intervalRegex, function (match, text, href) {
-          const clean = match.replace(INTERNEXT_QUERY_BEFORE, "").replaceAll("'", "").split(' ')
-          const amount = clean[0]
-          const interval = clean[1]
-          const date = new Date()
-          const now = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
-            date.getUTCDate(), date.getUTCHours(),
-            date.getUTCMinutes(), date.getUTCSeconds());
-          return `BETWEEN '${new Date(now).toISOString()}' AND '${updateDate(new Date(now), parseInt(amount), interval).toISOString()}'`
-        });
-    }
-    where = where.replaceAll('< BETWEEN', ' BETWEEN').replaceAll('> BETWEEN', ' BETWEEN').replaceAll('<= BETWEEN', ' BETWEEN').replaceAll('>= BETWEEN', ' BETWEEN')
-
-    const date_field = spacedField(config.date_field)
-    const geography_code_field_name = spacedField(geoField)
-    let aggregation_method = ''
-    let aggregation_field = ''
-    let aggregation_full = aggregation.replaceAll('(', '(data.')
+    const date_field = spacedField(config.date_field);
+    const geography_code_field_name = spacedField(geoField);
+    let aggregation_method = "";
+    let aggregation_field = "";
+    let aggregation_full = aggregation.replaceAll("(", "(data.");
     if (aggregation) {
-      aggregation_method = aggregation.split('(')[0]
-      if (aggregation.split('(')[1]) {
-        aggregation_field = aggregation.split('(')[1].replace(')', '')
+      aggregation_method = aggregation.split("(")[0];
+      if (aggregation.split("(")[1]) {
+        aggregation_field = aggregation.split("(")[1].replace(")", "");
       }
-      aggregation_full = aggregation_full.replace(aggregation_field, spacedField(aggregation_field))
-      aggregation_field = spacedField(aggregation_field)
+      aggregation_full = aggregation_full.replace(
+        aggregation_field,
+        spacedField(aggregation_field),
+      );
+      aggregation_field = spacedField(aggregation_field);
     }
     try {
       if (!date_field) {
-        const sql = `SELECT ${geography_code_field_name} as _geometry_code,
+        const sql =
+          `SELECT ${geography_code_field_name} as _geometry_code,
                             ${aggregation_full}          as _value,
                             MAX(data.concept_uuid)       as _concept_uuid,
                             MAX(data.admin_level)        as _admin_level
-                     FROM ? as data` + (where ? ` WHERE ${where}` : '') + ` GROUP BY ${geography_code_field_name}  ORDER BY ${geography_code_field_name} DESC`
+                     FROM ? as data` +
+          (where ? ` WHERE ${where}` : "") +
+          ` GROUP BY ${geography_code_field_name}  ORDER BY ${geography_code_field_name} DESC`;
         const results = alasql(sql, [data]).map((result, idx) => {
           return {
             id: idx,
@@ -127,73 +188,90 @@ export const getRelatedTableData = (data, config, selectedGlobalTime, geoField =
             concept_uuid: result._concept_uuid,
             date: null,
             value: result._value,
-          }
-        })
+          };
+        });
         cache[identifier] = {
-          rows: results
-        }
+          rows: results,
+        };
         return {
-          rows: results
-        }
+          rows: results,
+        };
       } else {
         let updatedWhere = where;
         if (selectedGlobalTime?.min) {
           if (updatedWhere) {
-            updatedWhere += ' AND '
+            updatedWhere += " AND ";
           }
-          updatedWhere += ` data.${date_field}>="${selectedGlobalTime?.min}"`
+          updatedWhere += ` data.${date_field}>="${selectedGlobalTime?.min}"`;
         }
         if (selectedGlobalTime?.max) {
           if (updatedWhere) {
-            updatedWhere += ' AND '
+            updatedWhere += " AND ";
           }
-          updatedWhere += ` data.${date_field}<="${selectedGlobalTime?.max}"`
+          updatedWhere += ` data.${date_field}<="${selectedGlobalTime?.max}"`;
         }
 
         // Update the date with ISO Format
-        data = data.map(row => {
-          if (typeof row[date_field] === 'string') {
-            const date = new Date(row[date_field])
+        data = data.map((row) => {
+          if (typeof row[date_field] === "string") {
+            const date = new Date(row[date_field]);
             if (isValidDate(date)) {
-              row[date_field] = date.toISOString().split('.')[0] + '+00:00'
+              row[date_field] = date.toISOString().split(".")[0] + "+00:00";
             }
           }
-          return row
-        })
+          return row;
+        });
         // -----------------------------------
         // Create SQL query
         // -----------------------------------
-        let sql = `SELECT ${geography_code_field_name}             as _geometry_code,` +
-          (aggregateDate ? `MAX(data.${date_field})` : `data.${date_field}`) + ` as _date,
+        let sql =
+          `SELECT ${geography_code_field_name}             as _geometry_code,` +
+          (aggregateDate ? `MAX(data.${date_field})` : `data.${date_field}`) +
+          ` as _date,
                           MAX(data.concept_uuid)                   as _concept_uuid,
                           MAX(data.geometry_code)                  as _ucode,
                           MAX(data.geometry_name)                  as _geometry_name,
                           ${aggregation_full} as _value,
                           MAX(data.admin_level)        as _admin_level
-                   FROM ? as data` + (updatedWhere ? ` WHERE ${updatedWhere}` : '') + ` GROUP BY ${geography_code_field_name}` + (!aggregateDate ? `, data.${date_field}` : '') + `  
-                   ORDER BY ${geography_code_field_name} DESC `
-        if (['MAJORITY', 'MINORITY'].includes(aggregation_method)) {
-          sql = `SELECT ${geography_code_field_name} as _geometry_code,` +
-            (aggregateDate ? `MAX(data.${date_field})` : `data.${date_field}`) + ` as _date,
+                   FROM ? as data` +
+          (updatedWhere ? ` WHERE ${updatedWhere}` : "") +
+          ` GROUP BY ${geography_code_field_name}` +
+          (!aggregateDate ? `, data.${date_field}` : "") +
+          `  
+                   ORDER BY ${geography_code_field_name} DESC `;
+        if (["MAJORITY", "MINORITY"].includes(aggregation_method)) {
+          sql =
+            `SELECT ${geography_code_field_name} as _geometry_code,` +
+            (aggregateDate ? `MAX(data.${date_field})` : `data.${date_field}`) +
+            ` as _date,
                         MAX(data.concept_uuid)       as _concept_uuid,
                         MAX(data.geometry_code)      as _ucode,
                         MAX(data.geometry_name)      as _geometry_name,
                         data.${aggregation_field}         as _value,
                         COUNT(${aggregation_field})  as value_occurrence,
                         MAX(data.admin_level)        as _admin_level
-                 FROM ? as data` + (updatedWhere ? ` WHERE ${updatedWhere}` : '') + ` GROUP BY ${geography_code_field_name}, data.${aggregation_field}` + (!aggregateDate ? `, data.${date_field}` : '') +
-            ` ORDER BY ${geography_code_field_name} DESC, value_occurrence ${aggregation_method === 'MAJORITY' ? 'DESC' : 'ASC'}`
+                 FROM ? as data` +
+            (updatedWhere ? ` WHERE ${updatedWhere}` : "") +
+            ` GROUP BY ${geography_code_field_name}, data.${aggregation_field}` +
+            (!aggregateDate ? `, data.${date_field}` : "") +
+            ` ORDER BY ${geography_code_field_name} DESC, value_occurrence ${aggregation_method === "MAJORITY" ? "DESC" : "ASC"}`;
         }
         if ([COUNT_UNIQUE].includes(aggregation_method)) {
-          sql = `SELECT ${geography_code_field_name}             as _geometry_code,` +
-            (aggregateDate ? `MAX(data.${date_field})` : `data.${date_field}`) + ` as _date,
+          sql =
+            `SELECT ${geography_code_field_name}             as _geometry_code,` +
+            (aggregateDate ? `MAX(data.${date_field})` : `data.${date_field}`) +
+            ` as _date,
                           MAX(data.concept_uuid)                   as _concept_uuid,
                           MAX(data.geometry_code)                   as _ucode,
                           MAX(data.geometry_name)                   as _geometry_name,
                           COUNT(DISTINCT(data.${aggregation_field})) as _value,
                           MAX(data.admin_level)        as _admin_level
-                   FROM ? as data` + (updatedWhere ? ` WHERE ${updatedWhere}` : '') + ` GROUP BY ${geography_code_field_name}` + (!aggregateDate ? `, data.${date_field}` : '') + `  
-                   ORDER BY ${geography_code_field_name} DESC `
+                   FROM ? as data` +
+            (updatedWhere ? ` WHERE ${updatedWhere}` : "") +
+            ` GROUP BY ${geography_code_field_name}` +
+            (!aggregateDate ? `, data.${date_field}` : "") +
+            `  
+                   ORDER BY ${geography_code_field_name} DESC `;
         }
         const results = alasql(sql, [data]).map((result, idx) => {
           return {
@@ -205,103 +283,102 @@ export const getRelatedTableData = (data, config, selectedGlobalTime, geoField =
             ucode: result._ucode,
             date: result._date,
             value: result._value,
-          }
-        })
+          };
+        });
         cache[identifier] = {
-          rows: results
-        }
+          rows: results,
+        };
         return {
-          rows: results
-        }
+          rows: results,
+        };
       }
     } catch (error) {
       return {
-        error: error.toString()
-      }
+        error: error.toString(),
+      };
     }
   } else {
     return {
-      rows: []
-    }
+      rows: [],
+    };
   }
-}
+};
 
 /***
  * Return geojson from related table data, based on lat and lon field, and a optional query
  */
-export const buildGeojsonFromRelatedData = (data, lon_field, lat_field, query = undefined) => {
-  const cleanData = deepClone(data)
-  cleanData.map(row => row._value = row.value)
-  let finalData = data
+export const buildGeojsonFromRelatedData = (
+  data,
+  lon_field,
+  lat_field,
+  query = undefined,
+) => {
+  const cleanData = deepClone(data);
+  cleanData.map((row) => (row._value = row.value));
+  let finalData = data;
   if (query) {
-    const where = query.replaceAll('"', '`').replace(/`(.*?)`/g, function (match, text, href) {
-      if (match.includes("'")) {
-        return match.replaceAll('`', '"')
-      }
-      return match
-    }).replaceAll('value', '_value');
+    const where = cleanWhereQuery(query);
     finalData = queryData(cleanData, where);
   }
 
-  const features = finalData.map(result => ({
-    type: 'Feature',
+  const features = finalData.map((result) => ({
+    type: "Feature",
     properties: result,
     geometry: {
-      type: 'Point',
-      coordinates: [result[lon_field], result[lat_field]]
-    }
+      type: "Point",
+      coordinates: [result[lon_field], result[lat_field]],
+    },
   }));
 
   const featureCollection = {
-    type: 'FeatureCollection',
-    features
-  }
+    type: "FeatureCollection",
+    features,
+  };
 
   return featureCollection;
 };
-
 
 /***
  * Return table fields ready for the query
  */
 export function getRelatedTableFields(relatedTable, relatedTableData) {
   if (relatedTable?.fields_definition) {
-    return relatedTable.fields_definition.map(field => {
-      let options = null
+    return relatedTable.fields_definition.map((field) => {
+      let options = null;
       if (relatedTableData) {
-        options = relatedTableData.map(
-          data => data[field.name] !== null ? '' + data[field.name] : ''
-        )
-        options = Array.from(new Set(options))
+        options = relatedTableData.map((data) =>
+          data[field.name] !== null ? "" + data[field.name] : "",
+        );
+        options = Array.from(new Set(options));
       }
-      field.type = field.type.toLowerCase()
-      if (!['number', 'date'].includes(field.type)) {
-        field.type = 'text'
+      field.type = field.type.toLowerCase();
+      if (!["number", "date"].includes(field.type)) {
+        field.type = "text";
       }
-      return { ...field, options: options }
-    })
+      return { ...field, options: options };
+    });
   } else {
-    return []
+    return [];
   }
 }
 
 /** Is key x value is date */
 export function isValueDate(key) {
   return (
-    key.toLowerCase().replaceAll('_', '').includes('date') ||
-    key.toLowerCase().replaceAll('_', '').includes('time')
-  )
+    key.toLowerCase().replaceAll("_", "").includes("date") ||
+    key.toLowerCase().replaceAll("_", "").includes("time")
+  );
 }
 
 /** Update related table response */
 export function updateRelatedTableResponse(response) {
-  response.map(row => {
+  response.map((row) => {
     for (const [key, value] of Object.entries(row)) {
-      const isDate = isValueDate(key)
+      const isDate = isValueDate(key);
       if (isDate && !isNaN(value)) {
-        row[key] = parseDateTime(value)
+        row[key] = parseDateTime(value);
       }
     }
-  })
-  return response
+  });
+  return response;
 }
