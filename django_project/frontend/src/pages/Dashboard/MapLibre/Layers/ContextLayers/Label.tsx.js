4040e0dd9a1ef29661a94b92827c39ab37ff662b
Irwan Fathurrahman <meomancer@gmail.com>
2026-03-20 16:20:42 +0700

    Fix cloud native append not working when layer is not exist (#629)
    * Fix cloud native append not working when layer is not exist
    
    * Remove text opacity on context layer label
    
    * Add missing fonts
    
    * fix flake

--- django_project/frontend/src/pages/Dashboard/MapLibre/Layers/ContextLayers/Label.tsx.js
+++ django_project/frontend/src/pages/Dashboard/MapLibre/Layers/ContextLayers/Label.tsx.js
@@ -20,7 +20,7 @@ export const renderContextLayerLabel = (sourceId, map, config, sourceLayer) => {
     source: sourceId,
     filter: ["==", "$type", "Point"],
     layout: layout,
-    paint: { ...paint, "text-opacity": 0.4 },
+    paint: { ...paint },
     maxzoom: maxZoom,
     minzoom: minZoom,
   };
--- django_project/geosight/data/api/v1/context_layer/detail/base_detail.py
+++ django_project/geosight/data/api/v1/context_layer/detail/base_detail.py
@@ -46,12 +46,8 @@ class ContextBaseDetailDataView(BaseApiV1ResourceReadOnly):
         )
         return obj
 
-    def get_context_layer_object(self, autocreate=False):
+    def get_context_layer_object(self):
         """Retrieve the linked cloud-native GIS for the current context layer.
-
-        :param autocreate:
-            If True, create a cloud-native GIS layer if none exists.
-        :type autocreate: bool
 
         :return: The associated cloud-native GIS layer object.
         :rtype: cloud_native_gis.models.layer.Layer
@@ -64,10 +60,6 @@ class ContextBaseDetailDataView(BaseApiV1ResourceReadOnly):
             if obj.cloud_native_gis_layer_id:
                 return Layer.objects.get(id=obj.cloud_native_gis_layer_id)
             else:
-                if not autocreate:
-                    raise ValueError(
-                        "The context layer does not have a cloud native layer."
-                    )
                 layer = Layer.objects.create(
                     name=obj.name,
                     layer_type=CloudNativeGISLayerType.VECTOR_TILE,
